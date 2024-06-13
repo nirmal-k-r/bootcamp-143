5835ddaa2668bea84e9f6ab57c46483f05f96a0a
Nirmal <Nirmal@admin.com>
2024-06-13 20:47:33 +0400

    new

---
+++ week15/calculator.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    print(a+b)  
+
+def avg(a,b):
+    print((a+b)/2)
+
+def sub(a,b):
+    print(a-b)
+
+def mul(a,b):
+    print(a*b)
+    
---
+++ week15/main.py
@@ -0,0 +1,18 @@
+from calculator import *
+# from calculator import add, sub
+
+sub(10,5)
+add(10,5)
+avg(10,5)
+mul(10,5)
+print('-----------------')
+
+import calculator
+calculator.add(10,10)
+calculator.sub(10,10)
+
+print('-----------------')
+
+import calculator as c
+c.add(20,20)
+c.sub(20,10)
