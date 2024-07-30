138f1a0a0fc097f1a7d36c15a8a2cbe7f51555c9
Nirmal <Nirmal@admin.com>
2024-07-30 19:19:18 +0400

    new

--- week20/day3/myapi/posts/urls.py
+++ week20/day3/myapi/posts/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from . import views
+from . import views 
+from .views import PostView
 
 urlpatterns = [
-    path('view',views.view_posts,name='view_posts'),
-    path('search',views.view_post_by_id,name='search posts'),
-    path('create',views.create_post,name='create post'),
-    path('delete',views.delete_post,name='delete post'),
-    path('update',views.update_post,name='update post')
+    path('', PostView.as_view(),name='view_posts'), #resouce for  all posts
+    path('<int:id>/', PostView.as_view(), name='post-detail'),
+    # path('search',views.view_post_by_id,name='search posts'),
+    # # path('create',views.create_post,name='create post'),
+    # path('delete',views.delete_post,name='delete post'),
+    # path('update',views.update_post,name='update post')
 ]
--- week20/day3/myapi/posts/views.py
+++ week20/day3/myapi/posts/views.py
@@ -2,37 +2,76 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 import json
 from .models import Post
 from .serialisers import PostSerializer
 
 
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 
-@csrf_exempt
-def view_posts(request):
-    if request.method == 'GET':
+class PostView(APIView): #resource that can be consumed - get and post
+    permission_classes = (AllowAny,)
+
+    def get(self, request, *args, **kwargs):
         data=Post.objects.all()
         serialiser=PostSerializer(data,many=True)
-        return JsonResponse(serialiser.data,safe=False)
-
-@csrf_exempt
-def view_post_by_id(request):
-    if request.method == 'POST':
-        data=json.loads(request.body) #parse the request body json
-        id=data['id'] #extract the id from the json
-        data=Post.objects.get(id=id) #get the post object with the id
-        serialiser=PostSerializer(data) #serialise the post object
-        return JsonResponse(serialiser.data,safe=False) #return the serialised data
+        return Response(serialiser.data)
     
-@csrf_exempt
-def create_post(request):
-    if request.method == 'POST':
-        data=json.loads(request.body) #parse the request body json
-        serialiser=PostSerializer(data=data) #serialise the data
+    
+    def post(self, request, *args, **kwargs):
+        serialiser=PostSerializer(data=request.data) #serialise the data
         if serialiser.is_valid(): #check if the serialised data is valid
             serialiser.save() #save the serialised data
-            return JsonResponse(serialiser.data,status=201) #return the serialised data with status code 201
-        return JsonResponse(serialiser.errors,status=400) #return the errors with status code 400
+            return Response(serialiser.data) #return the serialised data with status code 201
+        return Response(serialiser.errors) #return the errors with status code 400
+
+    def delete(self, request,id, *args, **kwarg):
+        article = Post.objects.get(pk=id)
+        article.delete()
+        return Response({"Status":"Done"},status=204)
+
+
+    def put(self, request, *args, **kwargs):
+        data=request.data
+        original=Post.objects.get(id=data['id'])
+        if 'title' in data:
+            original.title=data['title']
+        if 'content' in data:
+            original.content=data['content']
+        original.save()
+
+        return Response({'Status': 'Updated'},status=400)
+
+
+
+# @csrf_exempt
+# def view_posts(request):
+#     if request.method == 'GET':
+#         data=Post.objects.all()
+#         serialiser=PostSerializer(data,many=True)
+#         return JsonResponse(serialiser.data,safe=False)
+
+
+# @csrf_exempt
+# def view_post_by_id(request):
+#     if request.method == 'POST':
+#         data=json.loads(request.body) #parse the request body json
+#         id=data['id'] #extract the id from the json
+#         data=Post.objects.get(id=id) #get the post object with the id
+#         serialiser=PostSerializer(data) #serialise the post object
+#         return JsonResponse(serialiser.data,safe=False) #return the serialised data
+    
+# @csrf_exempt
+# def create_post(request):
+#     if request.method == 'POST':
+#         data=json.loads(request.body) #parse the request body json
+#         serialiser=PostSerializer(data=data) #serialise the data
+#         if serialiser.is_valid(): #check if the serialised data is valid
+#             serialiser.save() #save the serialised data
+#             return JsonResponse(serialiser.data,status=201) #return the serialised data with status code 201
+#         return JsonResponse(serialiser.errors,status=400) #return the errors with status code 400
     
 @csrf_exempt
 def delete_post(request):
