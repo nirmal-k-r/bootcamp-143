b0eeb6b3cd8aed5621c49a13d7a63b04324a9bed
Nirmal <Nirmal@admin.com>
2024-08-31 10:10:25 +0400

    new

---
+++ week15/day3/recap.js
@@ -0,0 +1,14 @@
+
+// age=parseFloat(18);
+
+// if (age===18){
+//     console.log("True");
+// }else{
+//     console.log("False");
+// }
+
+married=false;
+
+if (married===0){
+    console.log("Not married");
+}
